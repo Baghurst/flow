de64aabbf68c51c9a5f468e163fb5183983d2aa5
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-13 13:43:24 +0000

    Add files via upload

---
+++ FlowChartAndPythonExercise.py
@@ -0,0 +1,26 @@
+#1
+# Using For loop
+
+print("Number 1:")
+for A in range(0,11):
+    print(A)
+    
+# Using While loop
+
+    
+    
+    
+    
+    
+    
+#2
+# Using For loop
+    
+print("Number 2:")
+for A in range(11,0):
+    print(A)
+    
+# Using While loop    
+    
+    
+    
