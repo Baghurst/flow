12b39cab232421030085655c32d0d3ca9926ed4d
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-14 13:40:25 +0000

    Update FlowChartAndPythonExercise.py

--- FlowChartAndPythonExercise.py
+++ FlowChartAndPythonExercise.py
@@ -4,20 +4,16 @@
 print("Number 1:")
 
 print("For loop:")
-for A in range(11):
-    print(A)
+for A in range(11): # Printing for the begginning(0) to the number before 11(10), and storing it into A.
+    print(A) #Printing A
     
 # Using While loop
 
 print("While loop:")
-A = 0
-while A <= 10:
-    print(A)
-    A += 1
-    
-    
-    
-    
+A = 0 #The begginning value is 0.
+while A <= 10: # A is lower or equal to 10.
+    print(A) #prints A.
+    A += 1 #This means that it prints every number of the table of 1.  
     
 #2
 # Using For loop
@@ -25,31 +21,52 @@ while A <= 10:
 print("Number 2:")
 
 print("For loop:")
-for A in range(10,-1,-1):
-    print(A)
+for A in range(10,-1,-1): #prints from 10, stops before -1, and steps -1. And stores everything in A.
+    print(A) #Prints A
     
 # Using While loop    
 print("While loop:")
-A = 10
-while A >= 0:
-    print(A)
-    A -= 1
-    
+A = 10 #It means it starts from 10, the original value of A is 10.
+while A >= 0: #A is higher or euqual to 0, so it means to stop until it arrives to 0.
+    print(A) #Prints A.
+    A -= 1 #It means it prints downwards from the value of A(10)
+   
 #3
 # Using For loop
+
+print("Number 3:")
+
 print("For loop:")    
-for A in range(1,8):
-    print("#" * A)
-    A += 1    
+for A in range(1,8): #Prints from 1 to the number before 8(7)
+    print("#" * A) # "#" is multiplied by A.
+    A += 1 #It means it counts normally from smallest number to biggest.   
        
 # Using While loop   
 print("While loop:")
-A = 1   
-while A <= 7:
-    print("#" * A)
+A = 1 # A = 1 because if its 0 it dosen't do anything.    
+while A <= 7: # A is lower or equal to 7, so it means to go up to 7.
+    print("#" * A) # It prints "#" multiplied by A.
+    A += 1 #It means it counts normally from smallest number to biggest. 
+
+#4
+#NA
+    
+#5
+    
+print("Number 5:")
+
+# Using For loop
+print("For loop:")
+for A in range(0,11):
+    print(A "*" A, A "*" A)
     A += 1
-    
-    
+
+
+
+
+
+
+# Using While loop
     
     
     
