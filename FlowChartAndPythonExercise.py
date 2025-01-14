efad8adb85dd37aff1ae9b6794c0048b40c78243
Baghurst <23bbaghurst.acc@lwetb.ie>
2025-01-14 00:08:26 +0000

    Update FlowChartAndPythonExercise.py

--- FlowChartAndPythonExercise.py
+++ FlowChartAndPythonExercise.py
@@ -2,12 +2,18 @@
 # Using For loop
 
 print("Number 1:")
-for A in range(0,11):
+
+print("For loop:")
+for A in range(11):
     print(A)
     
 # Using While loop
 
-    
+print("While loop:")
+A = 0
+while A <= 10:
+    print(A)
+    A += 1
     
     
     
@@ -17,10 +23,40 @@ for A in range(0,11):
 # Using For loop
     
 print("Number 2:")
-for A in range(11,0):
+
+print("For loop:")
+for A in range(10,-1,-1):
     print(A)
     
 # Using While loop    
+print("While loop:")
+A = 10
+while A >= 0:
+    print(A)
+    A -= 1
+    
+#3
+# Using For loop
+print("For loop:")    
+for A in range(1,8):
+    print("#" * A)
+    A += 1    
+       
+# Using While loop   
+print("While loop:")
+A = 1   
+while A <= 7:
+    print("#" * A)
+    A += 1
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
